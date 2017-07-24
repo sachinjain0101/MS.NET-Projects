9e746cda19a7b4f5665b10802a14b4002371e134
sachinjain0101 <sachinjain.0101@gmail.com>
2017-07-24 02:38:36 -0400

    Quick Commit.

--- WordParser/WordParser/Kicker.cs
+++ WordParser/WordParser/Kicker.cs
@@ -16,6 +16,9 @@ using System.Threading.Tasks;
 namespace WordParser {
     class Kicker {
         private static ILog LOGGER = LogManager.GetLogger(typeof(Kicker));
+        const string REP_MATRIX = "{0}_report_matrix_{1}.csv";
+        const string TABLE_LIST = "{0}_report_tables_{1}.csv";
+        const string DATE_FORMAT = "yyyyMMddHHmmss";
 
         static void Main(string[] args) {
             XmlConfigurator.Configure();
@@ -42,45 +45,86 @@ namespace WordParser {
                 }
             }
 
-            string format = "yyyyMMddHHmmss";
-            String outFile = server + "_report_matrix_" + DateTime.Now.ToString(format) + ".csv";
+            //WriteFile(outFile, DataTableToCSV(dt, ','));
 
-            TextWriter tw = File.CreateText(outFile);
-            CsvWriter csv = new CsvWriter(tw);
+            var tables = dt.Rows.OfType<DataRow>()
+                    .Select(dr => dr.Field<string>("TablesUsed")).ToList();
 
-            foreach (DataColumn column in dt.Columns) {
-                csv.WriteField(column.ColumnName);
+            List<String> tblList = new List<string>();
+            foreach(String tbl in tables) {
+                if(!String.IsNullOrEmpty(tbl))
+                    tblList.AddRange(tbl.Split('|').ToList());
             }
-            csv.NextRecord();
+            WriteFile(string.Format(TABLE_LIST, server, DateTime.Now.ToString(DATE_FORMAT)), new SortedSet<string>(tblList));
 
-            foreach (DataRow row in dt.Rows) {
-                for (var i = 0; i < dt.Columns.Count; i++) {
-                    csv.WriteField(row[i]);
+            TextWriter tw = File.CreateText(string.Format(REP_MATRIX,server, DateTime.Now.ToString(DATE_FORMAT)));
+            using (CsvWriter csv = new CsvWriter(tw)) {
+
+                foreach (DataColumn column in dt.Columns) {
+                    csv.WriteField(column.ColumnName);
                 }
                 csv.NextRecord();
-            }
 
-            Console.ReadLine();
+                foreach (DataRow row in dt.Rows) {
+                    for (var i = 0; i < dt.Columns.Count; i++) {
+                        csv.WriteField(row[i]);
+                    }
+                    csv.NextRecord();
+                }
+            }
+            //Console.ReadLine();
+        }
+
+        static void WriteFile(String outFile, SortedSet<String> output) {
+            LOGGER.Info("Writing output script file");
+            System.IO.File.WriteAllLines(outFile, output);
+        }
+
+        static LinkedList<string> DataTableToCSV(DataTable dt, char seperator) {
+            LinkedList<string> lines = new LinkedList<string>();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < dt.Columns.Count; i++) {
+                sb.Append(dt.Columns[i]);
+                if (i < dt.Columns.Count - 1)
+                    sb.Append(seperator);
+            }
+            lines.AddLast(sb.ToString());
+
+            foreach (DataRow dr in dt.Rows) {
+                sb.Clear();
+                for (int i = 0; i < dt.Columns.Count; i++) {
+                    sb.Append(dr[i].ToString());
+
+                    if (i < dt.Columns.Count - 1)
+                        sb.Append(seperator);
+                }
+                lines.AddLast(sb.ToString());
+            }
+            return lines;
         }
 
         static string GetRelevantWords(String str) {
             SortedSet<string> filtered = new SortedSet<string>();
             foreach (string word in str.Split(' ')) {
-                if ((word.ToLower().StartsWith("tbl") 
-                    || word.ToLower().StartsWith("timecurrent..tbl") 
+                if ((word.ToLower().StartsWith("tbl")
+                    || word.ToLower().StartsWith("timecurrent..tbl")
                     || word.ToLower().StartsWith("timecurrent.dbo.tbl")
                     || word.ToLower().StartsWith("timehistory..tbl")
                     || word.ToLower().StartsWith("timehistory.dbo.tbl")
+                    || word.ToLower().StartsWith("schedular..tbl")
+                    || word.ToLower().StartsWith("schedular.dbo.tbl")
                     )
                     && !word.Contains(')')
                     && !word.Contains(',')
                     && word.Count(x => x == '.') < 3
                     )
-                    filtered.Add(word);
+                        filtered.Add(word.ToUpper());
             }
+            filtered.RemoveWhere(element => (element.ToLower().StartsWith("tbl") && element.Contains('.')));
             string outStr = "";
             foreach (string word in filtered)
                 outStr += word + "|";
+
             return outStr;
         }
 
@@ -88,14 +132,13 @@ namespace WordParser {
             Server srv = new Server(server);
             String script = "";
             foreach (string db in dbs.Split(',')) {
-                if (spName.Contains(db))
                     foreach (StoredProcedure sp in srv.Databases[db].StoredProcedures) {
                         if (spName.ToUpper().Contains(sp.Name.ToUpper())) {
                             script = "";
                             StringCollection lines = sp.Script();
 
                             foreach (String line in lines) {
-                                script = script + " " + line.Replace("\r\n", " ");
+                                script = script + " " + line.Replace("\r\n", " ").Replace("\t"," ").Replace("\"", " ").Replace(",", " ");
                             }
                         }
                     }
@@ -116,7 +159,7 @@ namespace WordParser {
                 qry += " SELECT A.ProgramName , ReportDesc , ExtendedDesc , ReportCode , REPLACE(StoredProcName,'..','.dbo.') as StoredProcName, B.Cnt ";
                 qry += " FROM Scheduler..tblReports2 A ";
                 qry += " JOIN (SELECT ProgramName , COUNT(1) AS Cnt FROM Scheduler..tblReports2 GROUP BY ProgramName) B ";
-                qry += " ON A.ProgramName = B.ProgramName ";
+                qry += " ON A.ProgramName = B.ProgramName "; // WHERE StoredProcName='usp_RPT_AgencyPayrateBillratebyEmplwDeptname'
                 qry += " ORDER BY B.Cnt DESC, A.ProgramName ";
 
                 DataTable dt = new DataTable();

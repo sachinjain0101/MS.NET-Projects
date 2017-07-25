42ca4af48ab42ff79049d4ac4f4bbf3234d76e56
sachinjain0101 <sachinjain.0101@gmail.com>
2017-07-25 17:45:23 -0400

    Enhanced XLS Handling

--- ExcelParser/ExcelParser/Kicker.cs
+++ ExcelParser/ExcelParser/Kicker.cs
@@ -12,48 +12,109 @@ namespace ExcelParser {
     class Kicker {
 
         const string DTL_STR = "%DTL-";
-        const string TMPL_ST_STR = "TemplateStart";
+        //const string TMPL_ST_STR = "TemplateStart";
         const string COL_END_NUM_STR = "ColEndNum";
         const string DB_STR = "Database";
         const string SP_STR = "StoredProc";
         const string DATE_FORMAT = "yyyyMMddHHmmss";
         const string SP_COLS = "{0}_{1}_{2}.csv";
+        const string DUMMY = "dummy_{0}.csv";
         const string COL_SEP = ",";
+        const string SPACE = " ";
+        const string NEWLINE = "\n";
+        const string DQUOTE = "\"";
+        const string SQUOTE = "'";
+        const string EQUALSTO = "=";
+        const string BLANK = "";
+        const string BANG = "!";
+        const string COMMA = ",";
+        const string PIPE = "|";
 
-        public static void Main(string[] args) {
-            getExcelFile();
+        public class XlData {
+            string _dbName;
+            string _spName;
+            List<string> _lines;
+
+            public string DbName { get => _dbName; set => _dbName = value; }
+            public string SpName { get => _spName; set => _spName = value; }
+            public List<string> Lines { get => _lines; set => _lines = value; }
         }
 
-        public static Dictionary<int, int> getCellPosition(Excel.Range xlRange, string searchStr) {
+        public static void Main(string[] args) {
+
+            Kicker k = new Kicker();
+            string inputDir = @"C:\Users\sachin.jain\Google Drive\#PeopleNet-Work\Reporting Project\Report Templates";
+
+            foreach (string file in System.IO.Directory.EnumerateFiles(inputDir)) {
+                Console.WriteLine("Processing: " + file);
+                XlData xld = k.getExcelData(file);
+                if (xld != null) {
+                    string outFile = string.Format(SP_COLS, xld.DbName, xld.SpName, DateTime.Now.ToString(DATE_FORMAT));
+                    System.IO.File.WriteAllLines(outFile, xld.Lines);
+                } else {
+                    string outFile = string.Format(DUMMY, DateTime.Now.ToString(DATE_FORMAT));
+                    System.IO.File.WriteAllLines(outFile, new List<string>() { "dummy" });
+                }
+            }
+
+
+        }
+
+        static Dictionary<int, int> getCellPosition(Excel.Range xlRange, string searchStr) {
             Dictionary<int, int> kv = new Dictionary<int, int>();
             Excel.Range xlSubRange = xlRange.Find(searchStr, Missing.Value, Excel.XlFindLookIn.xlValues);
+            if (xlSubRange == null)
+                return null;
             kv.Add(xlSubRange.Row, xlSubRange.Column);
             return kv;
         }
 
-        public static List<string> getColumnList(string dbName, string spName, Excel.Range xlRange, int colEndNum, int hdrStartNum, int dtlStartNum) {
+        static Dictionary<int, int> getHeaderCellPosition(Excel.Range xlRange) {
+            Dictionary<int, int> kv = new Dictionary<int, int>();
+            //report definition starts from 3
+            //and the header row will be available in the first 10 rows and we will check from 2nd column
+            bool found = false;
+            int J = 2;
+            LOOP_AGAIN:
+            for (int i = 3; i <= 10; i++) {
+                if (xlRange.Cells[i, J].Value2 != null) {
+                    kv.Add(i, 1);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found && J < 4) {
+                J += 1;
+                goto LOOP_AGAIN;
+            }
+            return kv;
+        }
+
+        static List<string> getColumnList(string dbName, string spName, Excel.Range xlRange, int colEndNum, int hdrStartNum, int dtlStartNum) {
             List<string> lines = new List<string>();
             for (int i = 1; i <= colEndNum; i++) {
                 if (xlRange.Cells[hdrStartNum, i].Value2 != null) {
-                    string h = xlRange.Cells[hdrStartNum, i].Value2.ToString();
-                    string d = xlRange.Cells[dtlStartNum, i].Value2.ToString();
-                    h = h.Replace('\n', ' ');
-                    d = d.Replace('\n', ' ');
+                    string h = (xlRange.Cells[hdrStartNum, i].Value2 == null) ? BLANK : xlRange.Cells[hdrStartNum, i].Value2.ToString();
+                    string d = (xlRange.Cells[dtlStartNum, i].Value2 == null) ? BLANK : xlRange.Cells[dtlStartNum, i].Value2.ToString();
 
-                    if (h.StartsWith("="))
-                        h = "'" + h;
-                    if (d.StartsWith("="))
-                        d = "'" + d;
+                    h = replaceStr(h, NEWLINE, SPACE);
+                    d = replaceStr(d, NEWLINE, SPACE);
 
-                    if (h.Contains("\""))
-                        h = h.Replace("\"","");
-                    if (d.Contains(","))
-                        d = d.Replace("\"", "");
+                    if (h.StartsWith(EQUALSTO))
+                        h = SQUOTE + h;
+                    if (d.StartsWith(EQUALSTO))
+                        d = SQUOTE + d;
 
-                    if (h.Contains(","))
-                        h = "\"" + h + "\"";
-                    if (d.Contains(","))
-                        d = "\"" + d + "\"";
+                    h = replaceStr(h, DQUOTE, BLANK);
+                    d = replaceStr(d, DQUOTE, BLANK);
+
+                    h = replaceStr(h, BANG, BLANK);
+                    d = replaceStr(d, BANG, BLANK);
+
+                    if (h.Contains(COMMA))
+                        h = DQUOTE + h + DQUOTE;
+                    if (d.Contains(COMMA))
+                        d = DQUOTE + d + DQUOTE;
 
                     lines.Add(dbName + COL_SEP + spName + COL_SEP + h + COL_SEP + d);
                 }
@@ -61,33 +122,44 @@ namespace ExcelParser {
             return lines;
         }
 
-        public static void getExcelFile() {
+        static string replaceStr(string inStr, string toFind, string toReplace) {
+            if (inStr.Contains(toFind))
+                inStr = inStr.Replace(toFind, toReplace);
+            return inStr;
+        }
+
+        public XlData getExcelData(string file) {
 
             //Create COM Objects. Create a COM object for everything that is referenced
             Excel.Application xlApp = new Excel.Application();
+            var oldSecurity = xlApp.AutomationSecurity;
+            xlApp.AutomationSecurity = Microsoft.Office.Core.MsoAutomationSecurity.msoAutomationSecurityForceDisable;
             xlApp.Application.DisplayAlerts = false;
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\sachin.jain\Google Drive\#PeopleNet-Work\Reporting Project\BillHour_BHEGByB11_SJ.XLS");
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(Filename: file, ReadOnly: true);
             Excel.Worksheet xlWorksheet = xlWorkbook.Sheets[1];
             Excel.Range xlRange = xlWorksheet.UsedRange;
 
+            xlRange.Rows.EntireRow.Hidden = false;
+            xlRange.Rows.EntireColumn.Hidden = false;
+
             Dictionary<int, int> kvDtl = getCellPosition(xlRange, DTL_STR);
-            Dictionary<int, int> kvTmplSt = getCellPosition(xlRange, TMPL_ST_STR);
+            Console.WriteLine("");
+            if (kvDtl == null)
+                return null;
+            Dictionary<int, int> kvHdrSt = getHeaderCellPosition(xlRange);
             Dictionary<int, int> kvColEndNum = getCellPosition(xlRange, COL_END_NUM_STR);
             Dictionary<int, int> kvDb = getCellPosition(xlRange, DB_STR);
             Dictionary<int, int> kvSp = getCellPosition(xlRange, SP_STR);
 
-            string dbName = xlRange.Cells[kvDb.ElementAt(0).Key + 1, kvDb.ElementAt(0).Value].Value2.ToString();
-            string spName = xlRange.Cells[kvSp.ElementAt(0).Key + 1, kvSp.ElementAt(0).Value].Value2.ToString();
+            XlData xld = new XlData();
+            xld.DbName = xlRange.Cells[kvDb.ElementAt(0).Key + 1, kvDb.ElementAt(0).Value].Value2.ToString();
+            xld.SpName = xlRange.Cells[kvSp.ElementAt(0).Key + 1, kvSp.ElementAt(0).Value].Value2.ToString();
 
             int colEndNum = Int32.Parse(xlRange.Cells[kvColEndNum.ElementAt(0).Key + 1, kvColEndNum.ElementAt(0).Value].Value2.ToString());
-            int hdr = (Int32.Parse(xlRange.Cells[kvTmplSt.ElementAt(0).Key + 1, kvTmplSt.ElementAt(0).Value].Value2.ToString())) - 1;
-            int dtl = Int32.Parse(kvDtl.ElementAt(0).Key.ToString());
+            int hdr = kvHdrSt.ElementAt(0).Key;
+            int dtl = kvDtl.ElementAt(0).Key;
 
-            List<string> lstCols = getColumnList(dbName, spName, xlRange, colEndNum, hdr, dtl);
-
-            string file = string.Format(SP_COLS, dbName, spName, DateTime.Now.ToString(DATE_FORMAT));
-
-            System.IO.File.WriteAllLines(file, lstCols);
+            xld.Lines = getColumnList(xld.DbName, xld.SpName, xlRange, colEndNum, hdr, dtl);
 
             //cleanup
             GC.Collect();
@@ -105,10 +177,14 @@ namespace ExcelParser {
             xlWorkbook.Close();
             Marshal.ReleaseComObject(xlWorkbook);
             xlApp.Application.DisplayAlerts = true;
+            xlApp.AutomationSecurity = oldSecurity;
 
             //quit and release
             xlApp.Quit();
             Marshal.ReleaseComObject(xlApp);
+
+            return xld;
+
         }
     }
 }

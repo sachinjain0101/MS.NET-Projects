6494289bab59e89115de3c84a81b448c84c35d48
sachinjain0101 <sachinjain.0101@gmail.com>
2017-08-02 17:12:37 -0400

    Test project for Regex

---
+++ TestRegularExpressions/TestRegularExpressions/Kicker.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestRegularExpressions {
+    class TestRegEx {
+        static void Main() {
+            string[] sentences =
+            {
+            "CREATE   TRIGGER ",
+            "CREATE TRIGGER ",
+            "create triggers",
+            "createtrigger",
+            "insert  into",
+            "insert ",
+            "insert into ",
+            "insertinto"
+            };
+
+            string[] sen = { "RecordId int", "RecordId  INT", "[RecordId] [int]","[RecordID]  [int]","[RecordID] [int]"};
+
+            string pattern = @"create[\s]+trigger\b|insert[\s]+into\b";
+
+            foreach (string s in sentences) {
+                System.Console.Write("{0,24}", s);
+
+                if (System.Text.RegularExpressions.Regex.IsMatch(s, pattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase)) {
+                    System.Console.WriteLine("  (match for '{0}' found)", pattern);
+                } else {
+                    System.Console.WriteLine();
+                }
+            }
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("");
+
+            string pat = @"recordid\b[\s]+int\b|\[recordid\b\][\s]+\[int\b\]";
+
+            foreach (string s in sen) {
+                System.Console.Write("{0,24}", s);
+
+                if (System.Text.RegularExpressions.Regex.IsMatch(s, pat, System.Text.RegularExpressions.RegexOptions.IgnoreCase)) {
+                    System.Console.WriteLine("  (match for '{0}' found)", pat);
+                    System.Console.WriteLine("{0,24}",Regex.Replace(s, pat, "[RECORDID] [bigint]", RegexOptions.IgnoreCase));
+                } else {
+                    System.Console.WriteLine();
+                }
+            }
+
+            // Keep the console window open in debug mode.
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+
+        }
+    }
+}

73f4c2e9bd64abbdace992b274b9b0fba0d67490
Sachin Jain <sachinjain.0101@gmail.com>
2017-11-15 12:32:55 -0500

    Datahub with Poller Windows Service

---
+++ DataHub/DataHub.Poller/Work.cs
@@ -0,0 +1,37 @@
+﻿
+using log4net;
+using System;
+using System.Reflection;
+using System.Timers;
+
+namespace DataHub.Poller {
+    class Work {
+        private static readonly ILog LOGGER = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        static Timer timer;
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
+            LOGGER.Fatal("Unhandled exception caught by AppDomain handler.  Application Terminating=" + e.IsTerminating, e.ExceptionObject as Exception);
+        }
+
+        public static void doWork() {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            LOGGER.Info("******* Service Started");
+            startTimer();
+            Console.ReadLine();
+        }
+
+        static void startTimer() {
+            LOGGER.Info("Timer started...");
+            double tickTime = 1000;
+            timer = new Timer(tickTime);
+            timer.Elapsed += new ElapsedEventHandler(timeElapsed);
+            timer.Start();
+        }
+
+        static void timeElapsed(object sender, ElapsedEventArgs e) {
+            LOGGER.Info("Hello World!!! - Performing scheduled task...");
+        }
+
+
+    }
+}
